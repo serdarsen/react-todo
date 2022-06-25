bca9f489d1c03de2cace1f9ff39381dda400d62c
serdarsen <serdarsendev@gmail.com>
2022-06-25 22:37:44 +0300

    Add main page. Add sass webpack support.

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+    return <div className="app__header">Hello!</div>;
+}
+
+export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,68 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin")
 
-module.exports = {
+const config = {
     mode: "development",
-    entry: path.join(__dirname, "src", "index.js"),
+    entry: path.join(__dirname, "src", "index.jsx"),
     output: {
         path:path.resolve(__dirname, "dist"),
+        filename: "main.[hash].js",
     },
+    devtool: "eval-cheap-source-map",
     devServer: {
         port: 8080,
         open: true,
+        hot: true
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env","@babel/preset-react"]
+                        presets: [
+                            ["@babel/preset-env", { "useBuiltIns": "usage", "corejs": 3, "targets": "defaults" }], 
+                            "@babel/preset-react"
+                        ]
                     }
                 },
-            },
+            }
         ]
+    },
+    resolve: {
+        extensions: [".jsx", ".js"]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
-        }),
+        })
     ],
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    }
+};
+
+
+if (currentTask == "build") {
+    config.mode = "production";
+    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
+    config.plugins.push(
+        new MiniCssExtractPlugin({ filename: "main.[hash].css" }), 
+        new CleanWebpackPlugin(), 
+        new WebpackManifestPlugin()
+    );
 }
+
+module.exports = config;
+
